5023faa25ca93780f56c9fa8e33c9c48802374f9
yosviel dominguez gonzalez <yosvield@gmail.com>
2023-10-30 08:36:27 -0400

    test: add test auth.service

--- src/app/core/services/auth.service.spec.ts
+++ src/app/core/services/auth.service.spec.ts
@@ -1,16 +1,31 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { AuthService } from './auth.service';
+import {AuthService} from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let storage: { [key: string]: string } = {};
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(AuthService);
+
+    spyOn(Object.getPrototypeOf(localStorage), 'getItem').and.callFake((key: string): string => {
+      return storage[key] || null;
+    });
+
+    spyOn(Object.getPrototypeOf(localStorage), 'setItem').and.callFake((key: string, value: string): string => {
+      return (storage[key] = value as string);
+    });
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should save token to local storage', () => {
+    const token = 'test-token';
+    service.setToken(token);
+    expect(service.getToken()).toEqual(token);
   });
 });
